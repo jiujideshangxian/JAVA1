7ed0d11b1248d9bb541b9011d648784adf82b0cc
jiujideshangxian <1529124973@qq.com>
2020-12-06 19:10:05 +0800

    java1

---
+++ src/com/btc/connect.java
@@ -0,0 +1,12 @@
+package com.btc;
+
+public class connect {
+    //maven:管理和构建项目的依赖和配置
+    //依赖的配置：xml文件
+    //eg:配置mysql数据库
+    //maven  |远端仓库
+    //       |本地仓库
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
